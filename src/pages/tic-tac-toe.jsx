09f1d88402920cb6cd3c8d8cb04eb3f304548798
WonTaeJang <jwt0212@gmail.com>
2024-02-29 20:49:48 +0900

    feat: tic-tac-toe game history 넣기위해 구조 변경

--- src/pages/tic-tac-toe.jsx
+++ src/pages/tic-tac-toe.jsx
@@ -19,7 +19,7 @@ function Square({value, onSquareClick}) {
   )
 }
 
-export default function Board() {
+const Board = () => {
   const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
@@ -89,5 +89,20 @@ function calculateWinner(squares) {
       return squares[a];
     }
   }
-  return null;
+  return null
+}
+
+export default function Game () {
+  return (
+    <>
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    </>
+  )
 }
